c789f852ebfb6b5e7be4a6bfb9b18653b22af57c
Hassan @Sungura <hmatar@ku.edu.tr>
2016-05-21 15:51:28 +0300

    Added windows to the room.

--- incs/Room.h
+++ incs/Room.h
@@ -21,7 +21,7 @@
 class Room: public Object {
   private:
     // Vertices of a unit cube centered at origin, sides aligned with axes
-    point4 vertices[16] = {
+    point4 vertices[32] = {
       // front wall
       point4( -0.5, -0.5,  0.5, 1.0 ), // bottom left
       point4( -0.5,  0.5,  0.5, 1.0 ), // top left
@@ -41,10 +41,35 @@ class Room: public Object {
       point4(  0.3, -0.5,  0.5, 1.0 ), // bottom right
 
       //rear wall
-      point4( -0.5, -0.5, -0.5, 1.0 ), // bottom left
-      point4( -0.5,  0.5, -0.5, 1.0 ), // top left
-      point4(  0.5,  0.5, -0.5, 1.0 ), // top right
-      point4(  0.5, -0.5, -0.5, 1.0 ), // bottom right
+      point4( -0.5,  0.5, -0.5, 1.0 ), // 12 top left
+      point4(  0.5,  0.5, -0.5, 1.0 ), // 13 top right
+      point4(  0.5,  0.4, -0.5, 1.0 ), // 14 top bottom right
+      point4( -0.5,  0.4, -0.5, 1.0 ), // 15 top bottom left
+
+      point4( -0.5, -0.5, -0.5, 1.0 ), // 16 bottom left
+      point4( -0.5,  0.3, -0.5, 1.0 ), // 17 bottom top left
+      point4(  0.5,  0.3, -0.5, 1.0 ), // 18 bottom top right
+      point4(  0.5, -0.5, -0.5, 1.0 ), // 19 bottom right
+
+      // left-side wall
+      point4( -0.5,  0.3, -0.5, 1.0), // rear mid-upper
+      point4( -0.5, -0.1, -0.5, 1.0), // rear mid-lower
+      point4( -0.5, -0.1,  0.5, 1.0), // front mid-lower
+      point4( -0.5,  0.3,  0.5, 1.0), // front mid-upper
+
+      //left side wall middle left face
+      point4( -0.5,  0.3, -0.4, 1.0), // 20
+      point4( -0.5, -0.1, -0.4, 1.0), // 21
+
+      // left side wall middle middle face
+      point4( -0.5,  0.3, -0.1, 1.0), // 22
+      point4( -0.5, -0.1, -0.1, 1.0), // 23
+      point4( -0.5,  0.3,  0.1, 1.0), // 24
+      point4( -0.5, -0.1,  0.1, 1.0), // 25
+
+      // left side wall middle right face
+      point4( -0.5,  0.3,  0.4, 1.0), // 26
+      point4( -0.5, -0.1,  0.4, 1.0), // 27
 
      /*,
       point4(  -1.0,  -1.0, 1.0, 1.0 ),
@@ -82,21 +107,71 @@ class Room: public Object {
     // generate 12 triangles: 36 vertices and 36 colors
     void colorcube() {
 
-      // constructing front wall
+      // Constructing front wall
       quad(1, 0, 8, 9);  // left face
       quad(4, 8, 11, 7); // bottom face
       quad(10, 11, 3, 2);  // right face
       quad(9, 5, 6, 10); // top face
 
-      // constructing right wall
-      quad( 2, 3, 15, 14 ); // right face
+      // Constructing the rear wall
+      quad( 12, 13, 14, 15 ); // upper face, A
 
-      quad( 12, 13, 14, 15 ); // rear
+      int start = vertexIndex;
+      quad( 14, 18, 18, 14 ); // middle left face, B
+      points[start+2].x -= 0.1;
+      points[start+4].x -= 0.1;
+      points[start+5].x -= 0.1;
 
-      quad( 13, 12, 0, 1 ); // left
-      //quad( 3, 0, 4, 7 ); // floor
+      start = vertexIndex;
+      quad( 14, 18, 18, 14 ); // middle middle left face, C
+      points[start+0].x -= 0.3;
+      points[start+1].x -= 0.3;
+      points[start+3].x -= 0.3;
+
+      points[start+2].x -= 0.4;
+      points[start+4].x -= 0.4;
+      points[start+5].x -= 0.4;
+
+      start = vertexIndex;
+      quad( 14, 18, 18, 14 ); // middle middle right face, D
+      points[start+0].x -= 0.6;
+      points[start+1].x -= 0.6;
+      points[start+3].x -= 0.6;
+
+      points[start+2].x -= 0.7;
+      points[start+4].x -= 0.7;
+      points[start+5].x -= 0.7;
+
+      start = vertexIndex;
+      quad( 15, 17, 17, 15 ); // middle middle right face, E
+      points[start+0].x += 0.1;
+      points[start+1].x += 0.1;
+      points[start+3].x += 0.1;
+
+      quad( 16, 17, 18, 19 ); // lower face, F
+
+      // Constructing the left wall
+      start = vertexIndex;
+      quad( 12, 20, 23, 1 ); // left upper face
+      quad( 20, 21, 25, 24 ); // left middle left face
+      quad( 26, 27, 29, 28 ); // left middle middle face
+      quad( 30, 31, 22, 23 ); // left middle right face
+      quad( 21, 16, 0, 22 ); // left lower face
+      int end = vertexIndex;
+
+      // Constructing the right wall
+      // It is the reflected rotation of the left wall.
+      for(; start < end; start++)
+      {
+        colors[vertexIndex] = black;
+        points[vertexIndex] = RotateY(180.0) * points[start];
+        vertexIndex++;
+      }
+
+      // Constructing the floor
+      quad( 3, 0, 16, 19 ); // floor
+
       //quad( 6, 5, 1, 2 ); // roof
-      //quad( 8, 9, 10, 11 );
     }
 
   public:
@@ -104,7 +179,7 @@ class Room: public Object {
      * Initializes the vertices and colors of the empty room object.
      */
     void initialize(GLuint program) {
-      numVertices = 42; // (7 faces)(2 triangles/face)(3 vertices/triangle)
+      numVertices = 126; // (21 faces)(2 triangles/face)(3 vertices/triangle)
       points = new point4[numVertices];
       colors = new color4[numVertices];
 
