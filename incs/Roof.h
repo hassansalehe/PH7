b98360a6264e1e7d75951ad00522d9f92ac3119d
Hassan @Sungura <hmatar@ku.edu.tr>
2016-05-22 14:43:12 +0300

    Nice, realistic roof.

--- incs/Roof.h
+++ incs/Roof.h
@@ -9,7 +9,7 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Implements the room of the museum
+// Implements the roof of the museum
 //
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -43,10 +43,63 @@ class Roof: public Object {
       colors[vertexIndex] = blue; points[vertexIndex] = vertices[d]; vertexIndex++;
     }
 
-    // generate 12 triangles: 36 vertices and 36 colors
+    /**
+     * Constructs one face(two triangles) of the roof sheet
+     */
+    void one_face( point4 lower, point4 upper, point4 third, point4 fourth ) {
+
+      colors[vertexIndex] = blue; points[vertexIndex] = lower; vertexIndex++;
+      colors[vertexIndex] = blue; points[vertexIndex] = upper; vertexIndex++;
+      colors[vertexIndex] = blue; points[vertexIndex] = third; vertexIndex++;
+      colors[vertexIndex] = blue; points[vertexIndex] = lower; vertexIndex++;
+      colors[vertexIndex] = blue; points[vertexIndex] = third; vertexIndex++;
+      colors[vertexIndex] = blue; points[vertexIndex] = fourth; vertexIndex++;
+    }
+
+    /**
+     * Constructs the sheets of the roof, with
+     */
+    void construct_sheets(point4 lower, point4 upper) {
+
+      float d = 0.02; // interval
+      point4 third; // third vertex
+      point4 fourth; // fourth vertex
+
+      int num = 0;
+      for(num = 0; num < 40; num++)
+      {
+        if( num % 2 ) { // is odd
+          third = upper - point4(0.0, 0.0, d, 0.0);
+          fourth = lower - point4(0.0, 0.0, d, 0.0);
+
+          one_face(lower, upper, third, fourth);
+
+          lower = fourth; upper = third;
+        }
+        else { // even
+          third = upper - point4(0.0, -d/2, d, 0.0);
+          fourth = lower - point4(0.0, -d/2, d, 0.0);
+          one_face(lower, upper, third, fourth);
+
+          lower -= point4(0.0, 0.0, 2*d, 0.0);
+          upper -= point4(0.0, 0.0, 2*d, 0.0);
+          one_face(fourth, third, upper, lower);
+        }
+      }
+    }
+
+    // generate 240 triangles: 720 vertices and 720 colors
     void colorcube() {
-      quad( 0, 1, 2, 3 ); // left roof
-      quad( 1, 4, 5, 2 ); // right roof
+
+      // Constructing the left roof
+      point4 lower = point4( -0.6,  0.45,  0.6, 1.0 );
+      point4 upper = point4(  0.0,  0.8,  0.6, 1.0 );
+      construct_sheets(lower, upper); // left roof
+
+      // Constructing the right roof
+      lower = point4( 0.6,  0.45,  0.6, 1.0 );
+      upper = point4(  0.0,  0.8,  0.6, 1.0 );
+      construct_sheets(upper, lower); // right roof
     }
 
   public:
@@ -54,7 +107,7 @@ class Roof: public Object {
      * Initializes the vertices and colors of the empty room object.
      */
     void initialize(GLuint program) {
-      numVertices = 12; //(2 faces)(2 triangles/face)(3 vertices/triangle)
+      numVertices = 720; //(120 faces)(2 triangles/face)(3 vertices/triangle)
       points = new point4[numVertices];
       colors = new color4[numVertices];
 
