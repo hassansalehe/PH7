f71dd994e09fa0ee665b473ab4af314cebd1c84c
Pirah <psoomro16@ku.edu.tr>
2016-05-30 15:52:58 +0300

    Added copy right Header

--- incs/Skull.h
+++ incs/Skull.h
@@ -1,10 +1,21 @@
-
-
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   COMP 510, Computer Graphics, Spring 2016
+//                              Final project
+//                PH7: A virtual Museum Based on OpenGL and Glut
+//
+//                            (c) 2016 - Hassan & Pirah.
+//            Copying without the authors consent is strictly prohibited.
+//
+///////////////////////////////////////////////////////////////////////////////
+//
 // read the vertices
 // read the colors
 // read the face indices
 // put the vertices in vertex array
 // put the colors in color array
+//
+///////////////////////////////////////////////////////////////////////////////
 
 // Scale the vertices
 // send them to the GPU
